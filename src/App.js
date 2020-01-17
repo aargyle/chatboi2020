035793a2cf2b5afc89349db61be78f576c699f1d
Alex Argyle <aargyle@uw.edu>
2020-01-17 09:04:38 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+    <Header />
+    <div className='bubble'>whattup!</div>
+    <TextArea onSend={t=> console.log(t)} />
+  </main>
+}
+
+function TextArea(props) {
+  const [text, setText] = useState('');
+  
+  return <div className='text-box'>
+  <input value={text} 
+    placeholder='Write your message'
+    onChange={e=> setText(e.target.value)}
+  />
+  <button onClick={() => {
+    props.onSend(text);
+    setText('');
+  }}>
+  Send
+  </button>
+  </div>
+}
+
+function Header() {
+  return <header>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png'/>
+    <h2> ChatBoi </h2>
+  </header>
 }
 
 export default App;
